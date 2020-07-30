1bba4d670f7098925bcee0ac4d3ea3af0e4296cd
Cathy Jin <Cathy.Jin@jncc.gov.uk>
2020-07-30 14:39:09 +0100

    Updating for new cookie banner

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -203,61 +203,87 @@ function initAjaxFiltering() {
 
 // Cookie Policy Banner
 (function ($) {
-    var $cookieBannerContainer = $('.js-cookie-banner-container');
-
-    if ($cookieBannerContainer.length === 0) {
-        return;
-    }
+    var $cookieBannerContainer = $('[data-cookie-banner-container]');
 
     var $bannerTemplate = $("#cookie-banner-template");
 
-    var cookieDuration = getDataAttributeValueOrDefault($cookieBannerContainer, "cookieDuration", 14);
-    var cookieName = getDataAttributeValueOrDefault($bannerTemplate, "cookieName", "complianceCookie");
-    var cookieValue = getDataAttributeValueOrDefault($bannerTemplate, "cookieValue", "true");
-
-    if (checkCookie(cookieName) === cookieValue || $bannerTemplate.length === 0) {
-        return;
-    }
-
     displayCookieBanner($cookieBannerContainer, $bannerTemplate);
-    createCookie(cookieName, cookieValue, cookieDuration);
-
-    function createCookie(name, value, days) {
-        if (days) {
-            var date = new Date();
-            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-            var expires = "; expires=" + date.toGMTString();
-
-            document.cookie = name + "=" + value + expires + "; path=/";
-        }
-    }
-
-    function checkCookie(name) {
-        var nameEQ = name + "=";
-        var ca = document.cookie.split(';');
-        for (var i = 0; i < ca.length; i++) {
-            var c = ca[i];
-            while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-            if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-        }
-        return null;
-    }
 
     function displayCookieBanner($container, $bannerTemplate) {
         var htmlContent = $bannerTemplate.text();
         $container.prepend(htmlContent);
     }
 
-    function getDataAttributeValueOrDefault($element, key, defaultValue) {
-        var dataAttribute = $element.data(key);
+})($);
 
-        if (dataAttribute === undefined) {
-            return defaultValue;
+
+function getDataAttributeValueOrDefault($element, key, defaultValue, invalidator) {
+    var dataAttribute = $element.data(key);
+
+    return dataAttribute;
+}
+function eraseCookie(name) {
+    createCookie(name, "", -1);
+}
+function createCookie(name, value, days) {
+
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        var expires = "; expires=" + date.toGMTString();
+        var domain = window.location.host;
+        var hasNumber = /\d/;
+
+        if (hasNumber.test(domain)) {
+            document.cookie = name + "=" + value + expires + "; path=/";
+        }
+        else
+        {
+            document.cookie = name + "=" + value + expires + "; domain=." + domain + "; path=/";
         }
 
-        return dataAttribute;
+       
     }
-})($);
+}
+
+function checkCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+function CookieAgreement() {
+    console.log("Cookies Agreed");
+    var $cookieBannerContainer = $('[data-cookie-banner-container]');
+
+    var $bannerTemplate = $("#cookie-banner-template");
+
+    var cookieDuration = 14;
+    var cookieName = "cookiePolicyAcceptance";
+    var cookieValue = "true";
+
+    createCookie(cookieName, cookieValue, cookieDuration);
+    location.reload();
+}
+
+function CookieDisagreement() {
+    console.log("Cookies Disagreed");
+    var $cookieBannerContainer = $('[data-cookie-banner-container]');
+
+    var $bannerTemplate = $("#cookie-banner-template");
+
+    var cookieDuration = 14;
+    var cookieName = "cookiePolicyAcceptance";
+    var cookieValue = "false";
+
+    createCookie(cookieName, cookieValue, cookieDuration);
+    location.reload();
+}
 
 // Expanding Tables
 (function ($) {
